896c05e7bcfe6446997c55a59ca69a26e3b21ef4
wwong <wwong@bouyguestelecom.fr>
2022-01-30 00:00:38 +0100

    bootstrap to helmet

--- FoodApi/index.js
+++ FoodApi/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const helmet = require("helmet")
+
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(helmet())
 
 app.listen(PORT, function() {
     console.log(`Express app is listening on port ${PORT}`)
