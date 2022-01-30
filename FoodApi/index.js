8e65cf24603eb0cbeb22fb2bf4cc81176f244f2c
waimanwong <waiman.wong@free.fr>
2022-01-30 01:21:43 +0100

    connect to mongodb

--- FoodApi/index.js
+++ FoodApi/index.js
@@ -3,6 +3,19 @@ const helmet = require("helmet")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 
+
+const mongoose = require("mongoose")
+const mongoString = "mongodb+srv://mongouser:mongouser@my-cluster.4uq9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(mongoString)
+mongoose.connection.on("error", function(error) {
+    if(process.env.NODE_ENV === "development") {
+        console.log(error)
+    }
+})
+mongoose.connection.on("open", function() {
+    console.log("Connected to MongoDB database")
+})
+
 const PORT = process.env.PORT || 5000
 const app = express()
 
